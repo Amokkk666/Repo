651cf52ee16beb468c0bdeddcc442be45cd905c4
Amokkk666 <alavrinok@ukr.net>
2023-10-24 12:36:45 +0300

    section_5

--- Section_5.py
+++ Section_5.py
@@ -9,3 +9,18 @@ if first_number == second_number == third_number:
 
 else:
     print ("Sum is: ",int(first_number)+int(second_number)+int(third_number))
+
+
+grade_input = input("What is your grade? ")
+grade = int(grade_input)
+if grade >= 90:
+    print("A")
+elif grade >=80 and grade<90:
+    print ("B")
+elif grade >=70 and grade<80:
+    print("C")
+elif grade >=60 and grade<70:
+    print("D")
+else:
+    print("F")
+    
