6b899da9441a3ee7aa4ec3aa4449870d858996f5
Amokkk666 <alavrinok@ukr.net>
2023-10-24 12:16:20 +0300

    section_5

---
+++ Section_5.py
@@ -0,0 +1,11 @@
+first_number = input ("What is the first number? ")
+second_number = input ("What is the second number? ")
+third_number = input ("What is the third number? ")
+
+if first_number == second_number == third_number:
+    print (int(first_number)+int(second_number)+int(third_number))
+    print (int(first_number)+int(second_number)+int(third_number))
+    print (int(first_number)+int(second_number)+int(third_number))
+
+else:
+    print (int(first_number)+int(second_number)+int(third_number))
