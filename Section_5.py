df9671cceb4ff2305b6b224e20932f8863caadfb
Amokkk666 <alavrinok@ukr.net>
2023-10-24 12:20:52 +0300

    Section_5

--- Section_5.py
+++ Section_5.py
@@ -3,9 +3,9 @@ second_number = input ("What is the second number? ")
 third_number = input ("What is the third number? ")
 
 if first_number == second_number == third_number:
-    print (int(first_number)+int(second_number)+int(third_number))
-    print (int(first_number)+int(second_number)+int(third_number))
-    print (int(first_number)+int(second_number)+int(third_number))
+    print ("Sum is: ",int(first_number)+int(second_number)+int(third_number))
+    print ("Sum is: ",int(first_number)+int(second_number)+int(third_number))
+    print ("Sum is: ",int(first_number)+int(second_number)+int(third_number))
 
 else:
-    print (int(first_number)+int(second_number)+int(third_number))
+    print ("Sum is: ",int(first_number)+int(second_number)+int(third_number))
