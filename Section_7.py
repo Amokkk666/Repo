c9d9e806a9c9fb8ebbf914f404422443ba623076
Amokkk666 <alavrinok@ukr.net>
2023-10-27 13:25:38 +0300

    section_7

--- Section_7.py
+++ Section_7.py
@@ -1,3 +1,4 @@
+#1. Exercise
 a = int(input("Enter the first integer: "))
 b = int(input("Enter the second integer: "))
 sign = input("Enter the sign (+ or -): ")
@@ -11,4 +12,27 @@ def calculation(a,b,sign):
     
 result = calculation (a,b,sign)
 print(result)
-    
+
+ #2. Exercise
+
+name = input("Enter the employee name: ")
+salary = input("Enter the employyee's salary: ")
+
+def showEmployee(name, salary):
+   return("Employee" + " " + name +" "+  "salary" +" "+ "is:" +" "+ salary )
+
+result=showEmployee(name, salary)
+print(result)
+
+ #3. Exercise
+
+a = int(input("Enter the first integer: "))
+b = int(input("Enter the second integer: "))
+
+def outerFun(a, b):
+    def innerFun(a, b):
+        return a+b
+    return innerFun(a, b) + 8
+
+result=outerFun(a,b)
+print(result)
